5ebb76063d4e4b4f631088f74ca2e009759687cb
oscarsosa1287 <oscarsosa1287@gmail.com>
2023-09-29 15:52:52 -0600

    formulario contacto
    formulario contacto y actionredirect

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -9,20 +9,29 @@ namespace Portafolio.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IRepositorioProyectos repositorioProyectos;
-       
+        private readonly IConfiguration configuration;
 
         public HomeController(ILogger<HomeController> logger,
-            IRepositorioProyectos repositorioProyectos)
+            IRepositorioProyectos repositorioProyectos,
+            IConfiguration configuration 
+            )
            
         {
             _logger = logger;
             this.repositorioProyectos = repositorioProyectos;
-          
+            this.configuration = configuration;
         }       
 
         public IActionResult Index()
         {
-            
+            var apellido = configuration.GetValue<string>("Apellido");
+            _logger.LogTrace("Este es un mensaje de trace");
+            _logger.LogDebug("Este es un mensaje de debug");
+            _logger.LogInformation("Este es un mensaje de information");
+            _logger.LogWarning("Este es un mensaje de warning");
+            _logger.LogError("Este es un mensaje de error");
+            _logger.LogCritical("Este es un mensaje de critical" +" " + apellido);
+
             var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();        
 
             var modelo = new HomeIndeViewModel()
@@ -32,9 +41,28 @@ namespace Portafolio.Controllers
             return View(modelo);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Proyectos()
         {
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+
+            return View(proyectos);
+        }
+
+        public IActionResult Contacto()
+        {         
+
             return View();
+        }
+        [HttpPost]
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
+        {
+            return RedirectToAction("Gracias");
+
+        }
+
+        public IActionResult Gracias()
+        {
+            return View(); 
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
